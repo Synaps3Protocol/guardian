5b683394847c9171f102a8393d524b65a5760c15
Geolffrey Mena <gmjun2000@gmail.com>
2024-12-02 20:53:58 -0600

    chore: enabled cache

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -192,9 +192,9 @@ func main() {
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		// ExposedHeaders:   []string{},
 		AllowCredentials: false,
 		// preflight cache maximum value not ignored by any of major browsers
 		MaxAge: 300,
